b3d63b4acc0cc7021dec543e2a30dc44e1226eb9
dhananjaypai08 <dhananjay2002pai@gmail.com>
2025-04-30 23:26:43 +0530

    api satellite location (online)

--- hardware_location/src/main.rs
+++ hardware_location/src/main.rs
@@ -1,7 +1,38 @@
-mod gps_location;
-mod errors;
-use gps_location::get_location;
+use reqwest::blocking::Client;
+use serde::Deserialize;
+use std::time::Duration;
+
+#[derive(Debug, Deserialize)]
+struct IpApiResponse {
+    lat: f64,
+    lon: f64,
+    city: String,
+    country: String,
+    query: String, // IP
+}
+
+fn get_ip_location() -> Result<IpApiResponse, Box<dyn std::error::Error>> {
+    let client = Client::builder()
+        .timeout(Duration::from_secs(5))
+        .user_agent("LocationFetcher/1.0")
+        .build()?;
+
+    let res = client
+        .get("http://ip-api.com/json/")
+        .send()?
+        .error_for_status()? // ensures HTTP 200
+        .json::<IpApiResponse>()?;
+
+    Ok(res)
+}
+
 fn main() {
-    println!("Hello, world!");
-    get_location();
+    match get_ip_location() {
+        Ok(loc) => {
+            println!("IP: {}", loc.query);
+            println!("City: {}, Country: {}", loc.city, loc.country);
+            println!("Latitude: {}, Longitude: {}", loc.lat, loc.lon);
+        }
+        Err(e) => eprintln!("Failed to fetch location: {}", e),
+    }
 }
